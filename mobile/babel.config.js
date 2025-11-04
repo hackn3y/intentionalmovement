91f6627a51b5e1708ecbcdd3c203311694c4b791
Ryan Hackney <hackn3y@gmail.com>
2025-11-03 22:42:11 -0500

    fix: Simplify babel config for Netlify compatibility

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,36 +2,6 @@ module.exports = function(api) {
   api.cache(true);
 
   return {
-    presets: [
-      ['babel-preset-expo', {
-        // Use modern JavaScript for web builds
-        web: {
-          useTransformReactJSXExperimental: true,
-        },
-      }],
-    ],
-    // Target modern browsers to reduce polyfills
-    env: {
-      production: {
-        presets: [
-          ['@babel/preset-env', {
-            targets: {
-              // Target browsers that support ES2015+ (removes most polyfills)
-              browsers: [
-                'last 2 Chrome versions',
-                'last 2 Firefox versions',
-                'last 2 Safari versions',
-                'last 2 Edge versions',
-              ],
-            },
-            // Don't transform modules (let bundler handle it)
-            modules: false,
-            // Only include polyfills that are actually used
-            useBuiltIns: 'usage',
-            corejs: 3,
-          }],
-        ],
-      },
-    },
+    presets: ['babel-preset-expo'],
   };
 };
